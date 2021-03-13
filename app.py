6a59455b1a6bd36b58a655d6abcbfb8b5b200d97
Your Name <typeyouremail@gmail.com>
2021-03-12 20:07:35 -0500

    Level 2: Add database to store messages and dashboard to display all

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from sense_hat import SenseHat 
+import sqlite3 as lite
+import sys
+
 
 sense = SenseHat()
 
@@ -12,9 +15,42 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def sent():
+    #get posted form data using names assigned in HTML
     message = request.form['message']
-    sense.show_message(message)
-    return render_template('sent.html', message=message)
+    name = request.form['name']
+    #generate string to display on Sense HAT
+    display = message + " Love, " + name
+
+    #connect to database and insert message and name
+    conn = lite.connect('./data/inspiration_messages.db')
+    curs=conn.cursor()
+    curs.execute("INSERT INTO messages values((?), (?))", (name, message))
+    conn.commit()
+
+    #close database connection
+    conn.close()
+
+    #display message on Sense HAT
+    sense.show_message(display)
+
+    #display success message to sending user 
+    return render_template('sent.html', message=message, name=name)
+
+@app.route('/all')
+def display_all():
+    #connect to database
+    conn = lite.connect('./data/inspiration_messages.db')
+    curs=conn.cursor()
+
+    #add all rows in messages table to messages list
+    messages = []
+    for row in curs.execute("SELECT * FROM messages"):
+        messages.append({'name': row[0], 'message': row[1]})
+    conn.close()
+
+    #render template showing messages
+    return render_template('all.html', messages=messages)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
